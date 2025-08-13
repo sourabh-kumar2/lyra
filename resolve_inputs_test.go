3e817218a2b227afc431573e0774818e01eb0b05
Sourabh Kumar <sourabhsingh0609@gmail.com>
2025-08-13 19:08:17 +0530

    read from task results

--- resolve_inputs_test.go
+++ resolve_inputs_test.go
@@ -33,7 +33,7 @@ func TestLyraResolveInputsContextOnly(t *testing.T) {
 	require.Implements(t, (*context.Context)(nil), ctxArg)
 }
 
-func TestLyra_ResolveInputs_RuntimeInput(t *testing.T) {
+func TestLyraResolveInputsRuntimeInput(t *testing.T) {
 	t.Parallel()
 
 	task, err := internal.NewTask("testTask",
@@ -52,4 +52,22 @@ func TestLyra_ResolveInputs_RuntimeInput(t *testing.T) {
 	// Verify userID value
 	userIDArg := args[1].Interface()
 	require.Equal(t, 123, userIDArg)
+}
+
+func TestLyraResolveInputsTaskResultInput(t *testing.T) {
+	t.Parallel()
+
+	task, err := internal.NewTask("consumer",
+		func(ctx context.Context, user string) (string, error) { return "processed", nil },
+		[]internal.InputSpec{Use("producer")})
+	require.NoError(t, err)
+
+	results := NewResult()
+	results.set("producer", "user_data") // Simulate previous task result
+
+	args, err := resolveInputs(context.Background(), task, results)
+
+	require.NoError(t, err)
+	require.Len(t, args, 2)
+	require.Equal(t, "user_data", args[1].Interface())
 }
